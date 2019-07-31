7be0dc077dc075c96ecc53e8780b676987ea42a7
SKosinski <stasiu.kosinski@hotmail.com>
2019-07-31 15:17:50 +0200

    Section 1 almost done

---
+++ Realm Rush/Assets/CubeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] [Range(1, 20)] int gridSize = 11;
+
+
+    void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        TextMesh cubeCords = GetComponentInChildren<TextMesh>();
+
+        string labelText = transform.position.x / gridSize + "," + transform.position.z / gridSize;
+
+        cubeCords.text = labelText;
+        gameObject.name = "Cube " + labelText;
+    }
+
+}
---
+++ Realm Rush/Assets/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+    [SerializeField] float waitTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        yield return new WaitForSeconds(waitTime);
+
+        foreach (Waypoint waypoint in path)
+        {
+            print("Visiting block: " + waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        print("Ending patrol.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
