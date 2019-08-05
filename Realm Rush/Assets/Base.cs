073aac8ca9380f5d1f22801eb26bbb9b28d46509
SKosinski <stasiu.kosinski@hotmail.com>
2019-08-05 17:52:12 +0200

    Pre PostProcessing

---
+++ Realm Rush/Assets/Base.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Base : MonoBehaviour
+{
+
+    [SerializeField] int baseHealth = 5;
+    public int score = 0;
+    [SerializeField] Text healthText;
+    [SerializeField] Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText.text = baseHealth.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeALife()
+    {
+        baseHealth--;
+        if (baseHealth == 0)
+        {
+            //ToDo end of game
+        }
+        healthText.text = baseHealth.ToString();
+    }
+
+    public void AddToScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+
+    }
+}
--- Realm Rush/Assets/Scripts/EnemyHealth.cs
+++ Realm Rush/Assets/Scripts/EnemyHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] ParticleSystem hitFX;
     [SerializeField] GameObject explosionFX;
     [SerializeField] int health = 200;
+    [SerializeField] int value = 20;
 
 
     void OnParticleCollision(GameObject other)
@@ -31,6 +33,7 @@ public class EnemyHealth : MonoBehaviour
 
     private void Death()
     {
+        FindObjectOfType<Base>().AddToScore(value);
         GameObject newDeathFX = Instantiate(deathFX, transform.position, Quaternion.identity);
         newDeathFX.transform.parent = FindObjectOfType<EnemySpawner>().transform;
         Destroy(gameObject);
@@ -38,6 +41,7 @@ public class EnemyHealth : MonoBehaviour
 
     public void Explode()
     {
+        FindObjectOfType<Base>().TakeALife();
         GameObject newExplosionFX = Instantiate(explosionFX, transform.position, Quaternion.identity);
         newExplosionFX.transform.parent = FindObjectOfType<EnemySpawner>().transform;
         Destroy(gameObject);
